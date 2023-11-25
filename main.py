1ec1c9eaf2ce10bf68f51cb6f95cb62c5222f88c
PLidiia <151846713+PLidiia@users.noreply.github.com>
2023-11-25 13:19:10 +0700

    coffee_bd

---
+++ main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+con = sqlite3.connect('coffee.bd')
+cur = con.cursor()
+# в виде ID представляются последние символов адреса данного кофе на сервисе яндекс маркет
+ID = ['924574', '1511150', '58573388']
+NAME_SORT = ['смесь арабики и робусты', 'смесь арабики и робусты', 'арабика']
+DEGREE_FRIED = ['светлая', 'очень тёмная', 'средняя']
+TYPE = ['seeds', 'milled', 'seeds']
+DESCRIPTION = ['Кофе в зернах MacCoffee Pure Espresso Forte', 'Кофе молотый Peppos Coffee CREMA E AROMA',
+               'Кофе для эспрессо Эфиопия Иргачефф Нат Tasty Coffee']
+PRICE = ['999', '384', '1605']
+VOLUME = ['1000', '250', '1000']
+for item in range(3):
+    cur.execute("INSERT INTO blocks (ID, name_sort, degree_fried, type, description, price, volume) "
+            "VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (ID[item], NAME_SORT[item], DEGREE_FRIED[item], TYPE[item], DESCRIPTION[item],
+             PRICE[item], VOLUME[item]))
+con.commit()
