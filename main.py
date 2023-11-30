8c5b3bd1d201c68b32be4d74d8f1437bef232e20
PLidiia <151846713+PLidiia@users.noreply.github.com>
2023-11-30 19:56:57 +0700

    new widget and update bd, ui files

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QStackedWidget
+
+from add_change import ADD_CHANGE
 
 
 class Widget(QWidget):
@@ -12,6 +14,12 @@ class Widget(QWidget):
         self.con = sqlite3.connect('coffee.bd')
         self.search_words.clicked.connect(self.searching)
         self.data_search.clicked.connect(self.pushing)
+        self.add_change.clicked.connect(self.adding_changing)
+
+    def adding_changing(self):
+        menu = ADD_CHANGE(widget)
+        widget.addWidget(menu)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def pushing(self):
         text_combo = self.atribute.currentText()
@@ -26,16 +34,18 @@ class Widget(QWidget):
             dates_one_name_descr = cur.execute("SELECT description FROM info WHERE id = ?", (text_search,)).fetchone()
             dates_one_price = cur.execute("SELECT price FROM info WHERE id = ?", (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE id = ?", (text_search,)).fetchone()
-            self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.count_data.setText(str(len(dates)))
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
         if text_combo == 'Название сорта':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE name_sort = ?", (text_search,)).fetchone()
+            print(dates_one_id[0])
             dates = cur.execute("SELECT ID FROM info WHERE name_sort = ?", (text_search,)).fetchall()
             dates_one_name_sort = cur.execute("SELECT name_sort FROM info WHERE name_sort = ?",
                                               (text_search,)).fetchone()
@@ -49,14 +59,16 @@ class Widget(QWidget):
                                           (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE name_sort = ?",
                                            (text_search,)).fetchone()
-            self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.count_data.setText(str(len(dates)))
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                print(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
         if text_combo == 'Степень обжарки':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE degree_fried = ?", (text_search,)).fetchone()
@@ -71,14 +83,15 @@ class Widget(QWidget):
                                                (text_search,)).fetchone()
             dates_one_price = cur.execute("SELECT price FROM info WHERE degree_fried = ?", (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE degree_fried = ?", (text_search,)).fetchone()
-            self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.count_data.setText(str(len(dates)))
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
         if text_combo == 'Молотый/в зернах':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE type = ?", (text_search,)).fetchone()
@@ -89,14 +102,15 @@ class Widget(QWidget):
             dates_one_name_descr = cur.execute("SELECT description FROM info WHERE type = ?", (text_search,)).fetchone()
             dates_one_price = cur.execute("SELECT price FROM info WHERE type = ?", (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE type = ?", (text_search,)).fetchone()
-            self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.count_data.setText(str(len(dates)))
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
         if text_combo == 'Описание вкуса':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE description = ?", (text_search,)).fetchone()
@@ -111,14 +125,15 @@ class Widget(QWidget):
                                                (text_search,)).fetchone()
             dates_one_price = cur.execute("SELECT price FROM info WHERE description = ?", (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE description = ?", (text_search,)).fetchone()
-            self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.count_data.setText(str(len(dates)))
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
         if text_combo == 'Цена':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE price = ?", (text_search,)).fetchone()
@@ -132,13 +147,14 @@ class Widget(QWidget):
             dates_one_price = cur.execute("SELECT price FROM info WHERE price = ?", (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE price = ?", (text_search,)).fetchone()
             self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
         if text_combo == 'Объём упаковки':
             dates_one_id = cur.execute("SELECT ID FROM info WHERE volume = ?", (text_search,)).fetchone()
@@ -152,13 +168,14 @@ class Widget(QWidget):
                                           (text_search,)).fetchone()
             dates_one_volume = cur.execute("SELECT volume FROM info WHERE volume = ?", (text_search,)).fetchone()
             self.count_data.setText(str(len(dates)))
-            self.id.setText(dates_one_id[0])
-            self.name.setText(dates_one_name_sort[0])
-            self.degree.setText(dates_one_degree[0])
-            self.type.setText(dates_one_type[0])
-            self.descript.setText(dates_one_name_descr[0])
-            self.price.setText(dates_one_price[0])
-            self.volume.setText(dates_one_volume[0])
+            if self.id and self.name and self.degree and self.type and self.descript and self.price and self.volume:
+                self.id.setText(str(dates_one_id[0]))
+                self.name.setText(dates_one_name_sort[0])
+                self.degree.setText(dates_one_degree[0])
+                self.type.setText(dates_one_type[0])
+                self.descript.setText(dates_one_name_descr[0])
+                self.price.setText(dates_one_price[0])
+                self.volume.setText(dates_one_volume[0])
 
     def searching(self):
         self.data_find.clear()
@@ -179,6 +196,10 @@ def except_hook(cls, exception, traceback):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Widget()
-    ex.show()
+    widget = QStackedWidget()
+    widget.addWidget(ex)
+    widget.setFixedWidth(1500)
+    widget.setFixedHeight(1500)
+    widget.show()
     sys.__excepthook__ = except_hook
     sys.exit(app.exec())
